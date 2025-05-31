c62955be69cd70c03efee79a4f4eece98aea5f5a
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-05-31 19:42:38 +0100

    exo-8

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-let nom: string;
-nom = "chaine de charactere"
+// let nom: string;
+// nom = "chaine de charactere"
 
-const age: number = 56
+// const age: number = 56
 
 const isYoung: boolean = true
 
@@ -20,3 +20,71 @@ let user: Users[] = [
         age: 56,
     }
 ]
+
+// ✅ 1. Déclaration de variables typées
+let nom: string = "chaine de charactere"
+let age: number = 100
+let estEtudiant: boolean = true
+
+// ✅ 2. Fonction saluer
+function saluer (prenom : string) : string {
+    return `Bonjour ${prenom} !`
+}
+
+// ✅ 3. Fonction addition
+function addition (a: number, b: number) : number {
+    return a + b
+}
+
+
+// ✅ 4. Interface Personne
+interface Personne {
+    nom: string
+    age: number
+}
+let utilisateur : Personne = {
+    nom: "Nehemie",
+    age: 56
+}
+
+// ✅ 5. Tableau typé + moyenne
+let notes: number[] = [10, 20, 30, 40]
+function calculerMoyenne (notes: number[]) : number {
+
+}
+
+// ✅ 6. Fonction avec union de types
+function afficherValeur (valeur : string | number) {
+    if (typeof valeur === "string") {
+        console.log("La valeur est de type String")
+    } else {
+        console.log("La valeur est de type Number")
+    }
+}
+
+// ✅ 7. Enum Jour + test week-end
+enum Jour {
+    Lundi, 
+    Mardi, 
+    Mercredi, 
+    Jeudi, 
+    Vendredi, 
+    Samedi, 
+    Dimanche
+}
+function estWeekend (jour: Jour) : boolean {
+    if (jour === Jour.Samedi) {
+        return true
+    } else if (jour === Jour.Dimanche) {
+        return false    
+    }
+}
+
+// ✅ 8. Fonction avec paramètre optionnel
+function direBonjour (nom?: string | undefined) {
+    if (nom) {
+        return `Bonjour, ${nom}`
+    } else {
+        return "Bonjour, invité"
+    }
+}
