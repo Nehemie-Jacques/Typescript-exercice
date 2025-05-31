6c0516e27114a89f7a85841c16c2d4615e26d64e
Votre Nom <Vous@exemple.com>
2025-05-31 19:34:36 +0200

    Initialisation du projet TypeScript
    Ajout de la configuration de base TypeScript : tsconfig.json, package.json, .gitignore et un fichier index.ts initial avec des exemples de typage.

---
+++ index.ts
@@ -0,0 +1,22 @@
+let nom: string;
+nom = "chaine de charactere"
+
+const age: number = 56
+
+const isYoung: boolean = true
+
+
+type Users = {
+    name: string
+    prename: string
+    age: number;
+    ville?: [any]
+}
+
+let user: Users[] = [
+    {
+        name: "rashid",
+        prename: "Haruna",
+        age: 56,
+    }
+]
